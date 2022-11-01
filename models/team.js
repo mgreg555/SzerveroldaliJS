c898e0bb326ed8059c2076b43e2b011b68b66907
mgreg555 <gergelymajor85@gmail.com>
2022-11-01 12:47:45 +0100

    5.feladat

--- models/team.js
+++ models/team.js
@@ -2,6 +2,7 @@ const Schema = require('mongoose').Schema;
 const db = require('../config/db');
 
 const Team = db.model('Team', {
+   id:String,
    name:String,
    stadium:String,
    bumberOfTrophies:Number,
